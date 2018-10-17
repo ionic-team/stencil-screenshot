417310288ed54d6e1acb75c48ef8f8c5c803468d
Adam Bradley <adambradley25@gmail.com>
2018-10-17 12:55:32 -0500

    fix routes

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -10,9 +10,11 @@ export class AppRoot {
   render() {
     return (
       <stencil-router class="full-screen">
-        <stencil-route url="/:buildIdA/:buildIdB" component="screenshot-compare" class="full-screen" />
-        <stencil-route url="/:buildId" exact={true} component="screenshot-preview" class="full-screen" />
-        <stencil-route url="/" component="screenshot-lookup" exact={true} class="full-screen" />
+        <stencil-route-switch>
+          <stencil-route url="/:buildIdA/:buildIdB" exact={true} component="screenshot-compare" class="full-screen" />
+          <stencil-route url="/:buildId" component="screenshot-preview" class="full-screen" />
+          <stencil-route url="/" component="screenshot-lookup" exact={true} class="full-screen" />
+        </stencil-route-switch>
       </stencil-router>
     );
   }
